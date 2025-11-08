02fe37a01f613434358097a31a4563fc2b543f8f
piste-boss <s.ishikawa@piste-i.com>
2025-11-08 20:00:35 +0900

    chore: switch functions to nft bundler

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -139,6 +139,8 @@ const mergeWithDefault = (config = {}, fallback = DEFAULT_CONFIG) => {
 }
 
 export const handler = async (event, context) => {
+  console.log('context keys', Object.keys(context || {}))
+  console.log('context.netlify', Boolean(context?.netlify))
   const store = getConfigStore(context)
 
   if (event.httpMethod === 'OPTIONS') {
